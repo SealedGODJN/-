64debb869f4878e7c7a21f01fb066f0b0677f6bd
SealedGODJN <1286039722@qq.com>
2020-04-19 12:33:00 +0800

    2020.4.19 修复了C编译器，测试C中的exit(0)和_exit(1)的不同，准备实现数据结构复习第二章的BinarySearch

---
+++ Review/chapter2/BinarySearch.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSIZE 10
+#define NotFound 0
+typedef int ElementType;
+
+typedef int Position;
+typedef struct LNode *List;
+struct LNode {
+    ElementType Data[MAXSIZE];
+    Position Last; /* 保存线性表中最后一个元素的位置 */
+};
+
+List ReadInput(); /* 裁判实现，细节不表。元素从下标1开始存储 */
+Position BinarySearch( List L, ElementType X );
+
+int main()
+{
+    List L;
+    ElementType X;
+    Position P;
+
+    L = ReadInput();
+    scanf("%d", &X);
+    P = BinarySearch( L, X );
+    printf("%d\n", P);
+
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+
+List ReadInput()
+{
+    List L = (List *)malloc(sizeof(List));
+    if(NULL == L)
+    {
+        printf("Malloc Failed!\n");
+        exit(1); // 表示了一个进程的结束
+    }
+
+}
+
+Position BinarySearch( List L, ElementType X )
+{
+
+}
