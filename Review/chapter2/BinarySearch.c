57906f059f77697b67dd56c53fe9e45192d62867
SealedGODJN <1286039722@qq.com>
2020-04-19 14:58:02 +0800

    2020.4.19 完成二分查找的编码和修改

--- Review/chapter2/BinarySearch.c
+++ Review/chapter2/BinarySearch.c
@@ -44,7 +44,7 @@ List ReadInput()
         exit(1); // 表示了一个进程的结束
     }
     L->Last = 5;
-    int temp[6] = {0, 5, 2, 6, 2, 1};
+    int temp[6] = {0, 1, 3, 5, 6, 7};
     for (int i = 0; i <= L->Last; i++)
     {
         L->Data[i] = temp[i];
@@ -52,18 +52,62 @@ List ReadInput()
     return L;
 }
 
+// /**
+//  * 返回二分查找的元素位置
+//  * @return:i 元素的位置
+//  */
+// Position search( List L, ElementType X, Position binary)
+// {
+//     if(binary )
+// }
+
 /**
  * 返回二分查找的元素位置
  * @return:i 元素的位置
  */
 Position BinarySearch( List L, ElementType X )
 {
-    for (int i = 1; i <= L->Last; i++)
+    int binary = 0, left = 1, right = L->Last;
+    while(left<=right)
     {
-        if(X == L->Data[i])
+        /**
+         * 错误：
+         * binary计算方法错误
+         */
+        // if(right%2 == 0)
+        // {
+        //     binary = right / 2;
+        // }
+        // else
+        // {
+        //     binary = (right + 1) / 2; // 记得要加括号
+        // }
+
+        binary = (left + right) / 2;
+        if(X > L->Data[binary]) 
         {
-            return i;
+            left = binary + 1;
         }
+        else if (X < L->Data[binary])
+        {
+            right = binary - 1;
+        }
+        else if (X == L->Data[binary])
+        {
+            return binary;
+        }
+
+        /**
+         * 错误：
+         * 想复杂了，不需要循环去遍历数组
+         */
+        // for (int i = left; i <= right; i++)
+        // {
+        //     if(X == L->Data[i])
+        //     {
+        //         return i;
+        //     }
+        // }
     }
     return NotFound;
 }
