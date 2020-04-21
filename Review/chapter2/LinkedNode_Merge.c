9ab6bf348466a46323f599aa6cef7c3e462022a3
SealedGODJN <1286039722@qq.com>
2020-04-21 11:27:47 +0800

    2020.4.20 数据结构复习——编程题目“两个有序链表序列的合并”未通过测试
    问题：
    指针操作出现问题，指针没有真正移动到L中
    
    解决方案：
    测试指针的移动和复制

--- Review/chapter2/LinkedNode_Merge.c
+++ Review/chapter2/LinkedNode_Merge.c
@@ -23,8 +23,8 @@ int main()
     List L1, L2, L;
     L1 = Read();
     L2 = Read();
-    // L = Merge(L1, L2);
-    // Print(L);
+    L = Merge(L1, L2);
+    Print(L);
     Print(L1);
     Print(L2);
     return 0;
@@ -62,6 +62,11 @@ List Read()
     {
         scanf("%d", &temp);
         List l = (List)malloc(sizeof(List));
+        if(NULL == l)
+        {
+            printf("头节点 Malloc Failed!\n");
+            exit(1); // 表示了一个进程的结束
+        }
         l->Data = temp;
         (*temp_l)->Next = l;
         *temp_l = l;
@@ -109,13 +114,102 @@ void Print( List L )
 
 /**
  * 将L1和L2合并为一个非递减的整数序列。应直接使用原序列中的结点，返回归并后的带头结点的链表头指针。
- * @arg: L1 L2
+ * @arg: L1 L2 L1和L2是给定的带头结点的单链表，其结点存储的数据是递增有序的
  */
 List Merge(List L1, List L2)
 {
+    if(L1 == NULL)
+    {
+        return L2;
+    }
+    if(L2 == NULL)
+    {
+        return L1;
+    }
     List L = (List)malloc(sizeof(List));
     if(NULL == L)
     {
-        
+        printf("中间变量 Malloc Failed!\n");
+        exit(1); // 表示了一个进程的结束
     }
+
+    List *nextNode = (List*)malloc(sizeof(List));
+    if(NULL == *nextNode)
+    {
+        printf("中间变量 Malloc Failed!\n");
+        exit(1); // 表示了一个进程的结束
+    }
+    *nextNode = L1;
+
+    // List *preNode = (List*)malloc(sizeof(List));
+    // if(NULL == *preNode)
+    // {
+    //     printf("中间变量 Malloc Failed!\n");
+    //     exit(1); // 表示了一个进程的结束
+    // }
+    // *preNode = L1;
+
+    List *temp_1 = (List*)malloc(sizeof(List));
+    if(NULL == *temp_1)
+    {
+        printf("中间变量 Malloc Failed!\n");
+        exit(1); // 表示了一个进程的结束
+    }
+    *temp_1 = L;
+
+    List *temp_2 = (List*)malloc(sizeof(List));
+    if(NULL == *temp_2)
+    {
+        printf("中间变量 Malloc Failed!\n");
+        exit(1); // 表示了一个进程的结束
+    }
+    *temp_2 = L2;
+    while((*nextNode) != NULL)
+    {
+        while((*temp_2) != NULL)
+        {
+            if((*temp_2)->Data <= (*nextNode)->Data)
+            {
+                (*temp_1)->Data = (*temp_2)->Data;
+                (*temp_1) = *temp_2;
+                (*temp_1) = (*temp_1)->Next;
+                (*temp_2) = (*temp_2)->Next;
+                // (*temp_1)->Next = NULL; // 有问题
+            }
+            else
+            {  
+               (*temp_2) = (*temp_2)->Next;
+            }
+            
+        }
+        (*temp_1)->Data = (*nextNode)->Data;
+        (*temp_1) = *nextNode;
+        (*temp_1) = (*temp_1)->Next;
+        *nextNode = (*nextNode)->Next;
+        // (*temp_1)->Next = NULL; // 有问题
+    }
+    return L;
+    // while((*nextNode) != NULL)
+    // {
+    //     printf("%d", (*nextNode)->Data);
+    //     *preNode = *nextNode;
+    //     *nextNode = (*nextNode)->Next;
+    //     while((*temp_2) != NULL)
+    //     {
+    //         if((*temp_2)->Data <=(*nextNode)->Data)
+    //         { // 插入节点
+    //             List *temp = (List*)malloc(sizeof(List));
+    //             if(NULL == *temp)
+    //             {
+    //                 printf("中间变量 Malloc Failed!\n");
+    //                 exit(1); // 表示了一个进程的结束
+    //             }
+    //             *temp = (*temp_2)->Next; // 保存当前节点的信息
+    //             (*preNode)->Next = *temp_2;
+    //             (*temp_2)->Next = *nextNode;
+    //             *temp_2 = *temp;
+    //         }
+    //         else *temp_2 = (*temp_2)->Next;
+    //     }
+    // }   
 }
--- Review/chapter2/test.c
+++ Review/chapter2/test.c
@@ -1,12 +1,22 @@
+/**
+ * @author hjn
+ * @time:2020.4.21
+ * 测试编译原理——语义分析，类型检查，类型相容问题的检查（比如：浮点型和整型数能否相加）
+ */
+
 #include <stdio.h>
 
 int main() {
-    int a[10];
-    for (int i = 0; i <= 9; i++) {
-        a[i] = i;
-    }
-    for (int i = 0; i <= 9; i++) {
-        printf("%d\n", a[i]);
-    }
+    // int a[10];
+    int a = 10;
+    float b = 2.5;
+    float c = b + a;
+    printf("%f\n", c);
+    // for (int i = 0; i <= 9; i++) {
+    //     a[i] = i;
+    // }
+    // for (int i = 0; i <= 9; i++) {
+    //     printf("%d\n", a[i]);
+    // }
     return 0;
 }
