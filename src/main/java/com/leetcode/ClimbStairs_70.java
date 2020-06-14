04abc781027155417526575276d27c8b8fe70637
SealedGODJN <1286039722@qq.com>
2020-06-14 23:55:11 +0800

    2020.6.14 leetcode，70.爬楼梯（AC）采用官方的从前往后递推方程

--- src/main/java/com/leetcode/ClimbStairs_70.java
+++ src/main/java/com/leetcode/ClimbStairs_70.java
@@ -13,32 +13,40 @@ import java.util.Scanner;
  */
 public class ClimbStairs_70 {
     public static int climbStairs(int n) {
-        int sum = 0;
-        int temp = 0;
-        int epoch = n/2;
-        for(int i=0; i <= epoch; i++) {
-            if(i*2 != n) {
-                sum += A(i,(n-i*2)+1);
-            } else {
-                sum += 1;
-            }
+        int f0 = 0;
+        int f1 = 1;
+        int f2 = 0;
+        for(int i=1; i<=n; i++) {
+            f2 = f0 + f1;
+            f0 = f1;
+            f1 = f2;
         }
-        return sum;
+        return f2;
+//        int temp = 0;
+//        int epoch = n/2;
+//        for(int i=0; i <= epoch; i++) {
+//            if(i*2 != n) {
+//                sum += A(i,(n-i*2)+1);
+//            } else {
+//                sum += 1;
+//            }
+//        }
+//        return sum;
     }
-    public static int A(int i, int n) {
-        if(i >= n) {
-            int temp = i;
-            i = n - 1;
-            n = temp + 1;
-        }
-        int sum = 1;
-        int j=i, k=n;
-        for(; j>0; j--) {
-            sum *= k;
-            k=k-1;
-        }
-        return sum;
-    }
+//    public static int A(int i, int n) {
+//        if(i >= n) {
+//            int temp = i;
+//            i = n - 1;
+//            n = temp + 1;
+//        }
+//        int sum = 1;
+//        int j=i, k=n;
+//        for(; j>0; j--) {
+//            sum *= k;
+//            k=k-1;
+//        }
+//        return sum;
+//    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
