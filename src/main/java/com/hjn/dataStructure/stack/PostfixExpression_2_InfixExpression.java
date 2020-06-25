badccec8d5051228d274cf5e43d88299af1bab15
SealedGODJN <1286039722@qq.com>
2020-06-26 01:09:25 +0800

    2020.6.25 Stack应用：中缀表达式转换为后缀表达式，错误：在pop出letter中的元素之后，不应该更新'('的优先级（只有出来的时候才需要更新其优先级）

--- src/main/java/com/hjn/dataStructure/stack/PostfixExpression_2_InfixExpression.java
+++ src/main/java/com/hjn/dataStructure/stack/PostfixExpression_2_InfixExpression.java
@@ -7,7 +7,8 @@ import java.util.Stack;
 public class PostfixExpression_2_InfixExpression {
     // 用于存储运算符的优先级，key为运算符，value为对应的优先级分数（value越大，优先级越高）
     public static HashMap<Character, Integer> letter_score = new HashMap<Character, Integer>(6);
-    public static ArrayList<Character> ie = new ArrayList<Character>();
+    public static Stack<Character> letter = new Stack<Character>();
+    public static Stack<Character> num = new Stack<Character>();
 
     public static void main(String[] args) {
 //        // 测试数字的ASCII范围
@@ -19,99 +20,62 @@ public class PostfixExpression_2_InfixExpression {
         letter_score.put('+', 1);
         letter_score.put('-', 1);
         letter_score.put('*', 2);
-        letter_score.put('/', 1);
+        letter_score.put('/', 2);
         letter_score.put('(', 3);
         letter_score.put(')', 3);
 
         // 测试
-        String test= "a*(b+c)";
+        String test= "2*(9+6/3-5)+4";
         char[] pe = test.toCharArray();
         pe2ie(pe);
-        for(Character c:ie) {
+        for(Character c:num) {
             System.out.print(c);
         }
     }
 
     public static void pe2ie(char[] pe) {
-        Stack<Character> letter = new Stack<Character>();
-        Stack<Character> num = new Stack<Character>();
 
         for(char c:pe) {
             if(c!='+' && c!='-' && c!='*' && c!='/' && c!='(' && c!= ')') {
                 num.push(c);
             } else {
                 if(letter.size() != 0 && c!=')') {
-                    char compare_letter = letter.peek();
-                    int compare_letter_score = letter_score.get(compare_letter);
-                    int current_letter_score = letter_score.get(c);
-                    if(current_letter_score > compare_letter_score) { 
-                        letter.push(c);
-                        if(c == '(') {
-                            letter_score.replace('(', 0);
-                        }
-                    } else { // 符号栈的运算符的优先级更高
-                        // 处理符号栈的运算符
-                        if(letter.peek()!= '(' && letter.peek()!=')') {
-                            if(num.size() != 0) {
-                                ie.add(num.pop());
-                            } else {
-                                System.err.println("num.size()==0");
-                                break;
-                            }
-                            if(num.size() != 0) {
-                                ie.add(num.pop());
-                            } else {
-                                System.err.println("num.size()==0");
-                                break;
-                            }
-                            ie.add(letter.pop());
+                    while (true) {
+                        char compare_letter;
+                        if(letter.size() != 0) {
+                            compare_letter = letter.peek();
                         } else {
-                            letter.pop();
-                        }
-                        if(letter.peek() == '(') {
-                            letter_score.replace('(', 3);
+                            letter.push(c);
+                            if(c == '(') {
+                                letter_score.replace('(', 0);
+                            }
+                            break;
                         }
 
-                        // 处理当前的运算符
-                        if(c!= '(' && c!=')') {
-                            if(num.size() != 0) {
-                                ie.add(num.pop());
-                            } else {
-                                System.err.println("num.size()==0");
-                                break;
+                        int compare_letter_score = letter_score.get(compare_letter);
+                        int current_letter_score = letter_score.get(c);
+                        if(current_letter_score > compare_letter_score) {
+                            letter.push(c);
+                            if(c == '(') {
+                                letter_score.replace('(', 0);
                             }
-                            if(num.size() != 0) {
-                                ie.add(num.pop());
+                            break;
+                        } else { // 符号栈的运算符的优先级更高
+                            // 处理符号栈的运算符
+                            if(letter.peek()!= '(' && letter.peek()!=')') {
+                                num.push(letter.pop());
                             } else {
-                                System.err.println("num.size()==0");
-                                break;
+                                letter.pop();
                             }
-                            ie.add(c);
-                        } else {
-                            letter.pop();
-                        }
-                        if(c == '(') {
-                            letter_score.replace('(', 3);
                         }
                     }
+
                 } else if(letter.size() == 0){
                     letter.push(c);
                 } else if(c == ')') { // 遇到右括号，出栈，直到遇到左括号
                     while (true) {
                         if(letter.peek()!= '(') {
-                            if(num.size() != 0) {
-                                ie.add(num.pop());
-                            } else {
-                                System.err.println("num.size()==0");
-                                break;
-                            }
-                            if(num.size() != 0) {
-                                ie.add(num.pop());
-                            } else {
-                                System.err.println("num.size()==0");
-                                break;
-                            }
-                            ie.add(letter.pop());
+                            num.push(letter.pop());
                         } else {
                             letter.pop();
                             break;
@@ -128,20 +92,19 @@ public class PostfixExpression_2_InfixExpression {
         }
 
         while (true) {
-            Character c = letter.peek();
-            if(c!= '(' && c!=')') {
-                if(num.size() != 0) {
-                    ie.add(num.pop());
-                } else {
-                    System.err.println("num.size()==0");
-                    break;
+            if(letter.size() != 0) {
+                Character c = letter.peek();
+                if(c!= '(' && c!=')') {
+                    num.push(letter.pop());
+                    if(letter.size() == 0) {
+                        System.err.println("letter.size()==0");
+                        return;
+                    }
                 }
-                ie.add(letter.pop());
-                if(letter.size() == 0) {
-                    System.err.println("letter.size()==0");
-                    return;
-                }
+            } else {
+                break;
             }
+
         }
     }
 }
---
+++ src/main/java/com/leetcode/MinStack_155.java
@@ -0,0 +1,9 @@
+package com.leetcode;
+
+import java.util.Stack;
+
+public class MinStack_155 {
+    public static void main(String[] args) {
+        Stack<Integer> test = new Stack();
+    }
+}
