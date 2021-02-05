5f2c69d370898759fd8dc372d1aa3e2772e7584f
SealedGODJN <1286039722@qq.com>
2021-02-05 16:48:47 +0800

    华为云官网——自学： 目标：复习java规范 内容：复习“java基础语句”编程规范1-4，记笔记 时间：2021.2.5

--- src/main/java/com/java/review/huawei/Test.java
+++ src/main/java/com/java/review/huawei/Test.java
@@ -1,5 +1,54 @@
 package com.java.review.huawei;
 
 public class Test {
+    public static void main(String[] args) {
+        // default块执行结束后，如果没有break，仍然会执行下一个case
+//        switch (3){
+//            default:
+//                System.out.print(3);
+//            case 0:
+//                System.out.print(0);
+//                break;
+//            case 1:
+//                System.out.print(1);
+//        }
 
+        // 测试int整型相除
+//        System.out.print(6/10);
+
+            // SwitchBreak对象实例化
+//            Test switchBreak = new Test();
+//
+//            switchBreak.choosePhoneBreak(1);
+        if(2000000000f == 2000000045) {
+            System.out.println("true");
+        }
+
+
+    }
+
+//    /**
+//     * switch包含break语句的方法
+//     *
+//     * @param number
+//     *            分支判断用的数值
+//     */
+//    private void choosePhoneBreak(int number) {
+//        // 输出的结果值
+//        String phoneBrand = "phonebrandc!";
+//
+//        switch (number) {
+//            case 1:
+//                phoneBrand = "phonebranda!";
+//                System.out.println("You choose the " + phoneBrand);
+//            case 2:
+//                // TODO:给变量phoneBrand赋值，打印输出You choose the xx，xx指的是变量的值。
+//                // 打印输出You choose the phonebrandb!
+//                phoneBrand = "phonebrandb!";
+//                System.out.println("You choose the" + phoneBrand);
+//                break;
+//            default:
+//                System.out.println("You choose the " + phoneBrand);
+//        }
+//    }
 }
---
+++ 笔记/Java复习笔记.md
@@ -0,0 +1,16 @@
+# Java复习笔记
+
+
+
+## 1 switch语句
+
+知识点：
+
+1、switch语句最好包含default部分。如果没有default部分，当没有一个case能匹配传入的值（switch表达式中的值）时，switch语句无法处理该值（相当于这一块程序没用，没有输出）。
+
+2、例外：如果switch处理的值是enum类型的，且包含该类型的所有可能值的显示案例，则switch语句可以不包含default部分。
+
+
+
+
+
