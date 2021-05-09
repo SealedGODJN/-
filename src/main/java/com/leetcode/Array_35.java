d7cf56fadddba34358575bbd2bc6465b364371a8
SealedGODJN <1286039722@qq.com>
2021-05-09 17:38:25 +0800

    leetcode刷题 目标：准备华为实习生面试 已完成： 1、数组的顺序遍历算法 2、数组的二分查找算法

--- src/main/java/com/leetcode/Array_35.java
+++ src/main/java/com/leetcode/Array_35.java
@@ -1,6 +1,13 @@
 package com.leetcode;
 
 public class Array_35 {
+
+    /**
+     * 暴力解法
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int searchInsert(int[] nums, int target) {
         int last;
         int next;
@@ -28,9 +35,35 @@ public class Array_35 {
         return index;
     }
 
+    /**
+     * 二分法
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert_binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+
+        // 特例1
+        if(nums[0] > target) return 0;
+        // 特例2
+        if(nums[nums.length-1] < target) return nums.length;
+
+        while(low<=high) {
+            int mid = low+(high-low) / 2; // 一直困扰我的：偶数长度的数组和奇数长度的数组，mid的计算公式是否相同？
+
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low = mid +1;
+            else if(nums[mid] > target) high = mid-1;
+        }
+        return high+1;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 3, 5, 6};
-        int target = 8;
+        int target = 4;
         System.out.println(searchInsert(nums, target));
+        System.out.println(searchInsert_binarySearch(nums, target));
     }
 }
