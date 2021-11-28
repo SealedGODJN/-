fe7e6a424284ea2805fe0387ed9ef3d96e9ce222
SealedGodJn <1286039722@qq.com>
2021-11-28 18:02:39 +0800

    学习java多线程
    已完成：
    1、多线程的2-3
    2、完成一道leetcode，String28
    3、需要对Offer 5 进行修改
    
    待完成：
    1、改进反转字符串二，重构，思考哪里变慢了

--- src/main/java/com/leetcode/String_Offer_5.java
+++ src/main/java/com/leetcode/String_Offer_5.java
@@ -10,21 +10,31 @@ public class String_Offer_5 {
      * @return
      */
     public static String replaceSpace(String s) {
-        String[] sArray = s.split("\\s");
+        char[] sArray = s.toCharArray();
         int len = s.length();
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i=0; i<sArray.length; i++) {
-            stringBuilder.append(sArray[i]);
-
-            if (i != sArray.length - 1)
+        for (int i=0; i < len; i++) {
+            if (sArray[i] == ' ') {
                 stringBuilder.append("%20");
+            }
+            else stringBuilder.append(sArray[i]);
         }
 
         return stringBuilder.toString();
     }
 
+    /*
+输入：
+"     "
+输出：
+""
+预期结果：
+"%20%20%20%20%20"
+     */
+
     public static void main(String[] args) {
-        String s = "We are happy.";
+//        String s = "We are happy.";
+        String s = "   ";
         String result = replaceSpace(s);
         System.out.println(result);
     }
