41819ed546d528c3ae5e0b8a8ad083781e893c56
SealedGodJn <1286039722@qq.com>
2021-12-11 18:24:10 +0800

    学习java多线程
    已完成：
    1、多线程的2-3
    2、复习leetcode双指针27
    
    待完成：
    1、改进反转字符串二，重构，思考哪里变慢了

--- src/main/java/com/leetcode/Array_27.java
+++ src/main/java/com/leetcode/Array_27.java
@@ -41,20 +41,8 @@ public class Array_27 {
     public static void main(String[] args) {
         int[] nums = new int[]{0,1,2,2,3,0,4,2};
         int val = 2;
-        System.out.println(removeElement(nums, val));
-//        System.out.println(removeElement_two_pointers(nums, val));
+        System.out.println(removeElement(nums, val));//        System.out.println(removeElement_two_pointers(nums, val));
     }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
