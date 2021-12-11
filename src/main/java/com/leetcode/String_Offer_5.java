d86d983d4013385135c85f853a87741e35f0192b
hjn <1286039722@qq.com>
2021-12-11 18:41:14 +0800

    学习java多线程
    已完成：
    1、多线程的2-3
    2、复习剑指offer第五题，替换空格，完成空间复杂度为O(1)的算法
    
    待完成：
    1、改进反转字符串二，重构，思考哪里变慢了

--- src/main/java/com/leetcode/String_Offer_5.java
+++ src/main/java/com/leetcode/String_Offer_5.java
@@ -6,6 +6,8 @@ public class String_Offer_5 {
      * 不要使用split函数
      * 空间复杂度最好为1
      *
+     * 此方法的时间复杂度为
+     *
      * @param s
      * @return
      */
@@ -36,6 +38,24 @@ public class String_Offer_5 {
      *
      * @param s
      * @return
+     */
+//    public static String replaceSpace(String s) {
+//        char[] sArray = s.toCharArray();
+//        int len = s.length();
+//        StringBuilder stringBuilder = new StringBuilder();
+//        for (int i=0; i < len; i++) {
+//            if (sArray[i] == ' ') {
+//                stringBuilder.append("%20");
+//            }
+//            else stringBuilder.append(sArray[i]);
+//        }
+//
+//        return stringBuilder.toString();
+//    }
+
+    /**
+     * 完成空间复杂度为O（1）的算法
+     * @param s
      */
     public static String replaceSpace(String s) {
         char[] sArray = s.toCharArray();
