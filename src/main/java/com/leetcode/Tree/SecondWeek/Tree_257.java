8b0d8e014435ec8b6d26f15f8b0154ff957058fb
SealedGodJn <1286039722@qq.com>
2022-02-16 09:59:22 +0800

    leetcode+可视化
    已完成：
    1、多线程的2-3
    2、完成257：找出根节点到叶子节点的所有路径（递归【改进版】 | 回溯）
    
    待完成：
    1、复习java基础
    2、学习组合数学

--- src/main/java/com/leetcode/Tree/SecondWeek/Tree_257.java
+++ src/main/java/com/leetcode/Tree/SecondWeek/Tree_257.java
@@ -7,18 +7,64 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tree_257 {
-    List<List<Integer>> result = new ArrayList<>();
+//    List<List<Integer>> result = new ArrayList<>();
+//    public List<String> binaryTreePaths(TreeNode root) {
+//        List<String> allPath = new ArrayList<>();
+//        if (root == null) {
+//            return allPath;
+//        }
+//        List<Integer> path = new ArrayList<>();
+//        path.add(root.val);
+//
+//        traversal(root, path);
+//        for (List<Integer> paths : result) {
+//            StringBuilder sb = new StringBuilder();
+//            for (Integer integer : paths) {
+//                sb.append(integer).append("->");
+//            }
+//            sb.delete(sb.length() - 2, sb.length());
+//            allPath.add(sb.toString());
+//        }
+//        return allPath;
+//    }
+//
+//    /**
+//     * 递归遍历树
+//     * @param root 一棵树的根节点
+//     * @param path 根节点到叶子节点
+//     */
+//    public void traversal(TreeNode root, List<Integer> path) {
+//        if (root.left == null && root.right == null) {
+//            List<Integer> temp = new ArrayList<>(path);
+//            result.add(temp);
+//            return;
+//        }
+////        path.add(root.val);
+//
+//        if (root.left != null) {
+//            path.add(root.left.val);
+//            traversal(root.left, path);
+//            path.remove(path.size() - 1);
+//        }
+//
+//        if (root.right != null) {
+//            path.add(root.right.val);
+//            traversal(root.right, path);
+//            path.remove(path.size() - 1);
+//        }
+//    }
 
     /**
      * 1、明确递归函数的参数和返回值 <br>
-     * 参数：node
-     * 返回值：
+     * 参数：node path（记录每一条路径） result（存放结果集）
+     * 返回值：无
      *
      * 2、明确终止条件 <br>
+     * 遇到叶子节点
      *
-     *
-     * 3、单层逻辑 <br>
-     *
+     * 3、单次递归逻辑 <br>
+     * 1）终止条件的内部逻辑：应该把path(List<Integer>)转化为allPath中的内容
+     * 2）单次递归逻辑：对左右子树进行递归和回溯
      *
      * @param root
      * @return
@@ -29,44 +75,51 @@ public class Tree_257 {
             return allPath;
         }
         List<Integer> path = new ArrayList<>();
-        path.add(root.val);
+//        path.add(root.val);
 
-        traversal(root, path);
-        for (List<Integer> paths : result) {
-            StringBuilder sb = new StringBuilder();
-            for (Integer integer : paths) {
-                sb.append(integer).append("->");
-            }
-            sb.delete(sb.length() - 2, sb.length());
-            allPath.add(sb.toString());
-        }
+        traversal(root, path, allPath);
+//        for (List<Integer> paths : result) {
+//            StringBuilder sb = new StringBuilder();
+//            for (Integer integer : paths) {
+//                sb.append(integer).append("->");
+//            }
+//            sb.delete(sb.length() - 2, sb.length());
+//            allPath.add(sb.toString());
+//        }
         return allPath;
     }
 
     /**
-     * 递归遍历树
-     * @param root 一棵树的根节点
-     * @param path 根节点到叶子节点
+     * 递归遍历树的每一个节点
+     * @param root
+     * @param path
      */
-    public void traversal(TreeNode root, List<Integer> path) {
+    public void traversal(TreeNode root, List<Integer> path, List<String> result) {
+        path.add(root.val);
+
         if (root.left == null && root.right == null) {
-            List<Integer> temp = new ArrayList<>(path);
-            result.add(temp);
-            return;
+            StringBuilder sb = new StringBuilder();
+            for (Integer integer : path) {
+                sb.append(integer).append("->");
+            }
+            sb.delete(sb.length() - 2, sb.length());
+            result.add(sb.toString());
         }
-//        path.add(root.val);
 
         if (root.left != null) {
-            path.add(root.left.val);
-            traversal(root.left, path);
+//            path.add(root.left.val);
+            traversal(root.left, path, result);
+            // 回溯和递归是一一对应的，有一个递归，就要有一个回溯
             path.remove(path.size() - 1);
         }
 
         if (root.right != null) {
-            path.add(root.right.val);
-            traversal(root.right, path);
+//            path.add(root.right.val);
+            // 回溯和递归是一一对应的，有一个递归，就要有一个回溯
+            traversal(root.right, path, result);
             path.remove(path.size() - 1);
         }
+
     }
 
     public static void main(String[] args) {
