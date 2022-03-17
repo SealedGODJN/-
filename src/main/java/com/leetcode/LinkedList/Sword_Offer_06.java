17f4327063346b2b17299d673cae67966f798ed5
SealedGodJn <1286039722@qq.com>
2022-03-18 00:45:02 +0800

    leetcode+可视化
    已完成：
    1、多线程的2-3
    2、完成剑指Offer06【递归+非递归+头插入ArrayList】
    
    待完成：
    1、学习java的多线程技术
    2、

--- src/main/java/com/leetcode/LinkedList/Sword_Offer_06.java
+++ src/main/java/com/leetcode/LinkedList/Sword_Offer_06.java
@@ -1,6 +1,7 @@
 package com.leetcode.LinkedList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Sword_Offer_06 {
@@ -9,18 +10,36 @@ public class Sword_Offer_06 {
         ListNode next = null;
         ListNode(int val) { this.val = val; }
     }
+
+//    /**
+//     * 非递归
+//     * @param listNode
+//     * @return
+//     */
+//    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+//        if (listNode == null) return new ArrayList<>();
+//        ListNode temp = listNode;
+//        ArrayList<Integer> result = new ArrayList<>();
+//        while (temp != null) {
+//            result.add(0, temp.val);
+//            temp = temp.next;
+//        }
+//        return result;
+//    }
+
+    ArrayList<Integer> result = new ArrayList<>();
+    /**
+     *
+     * @param listNode
+     * @return
+     */
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        if (listNode == null) return new ArrayList<>();
-        ListNode temp = listNode;
-        Stack<Integer> record = new Stack<>();
-        while (temp != null) {
-            record.push(temp.val);
-            temp = temp.next;
-        }
-        ArrayList<Integer> result = new ArrayList<>();
-        while (!record.isEmpty()) {
-            result.add(record.pop());
+
+        if (listNode != null) {
+            printListFromTailToHead(listNode.next);
+            result.add(listNode.val);
         }
         return result;
     }
+
 }
