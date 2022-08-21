432272d30e4728a03788789fa107bc7dbd0bf7fe
SealedGodJn <1286039722@qq.com>
2022-08-21 23:35:30 +0800

    leetcode+可视化
    已完成：
    1、多线程的2-3
    2、Tree108还没写
    3、系统性学习dp，但是也不太想学
    
    待完成：
    1、学习java的多线程
    2、学习乐观锁、悲观锁的具体应用（预计4个小时）

--- src/main/java/com/leetcode/Tree/ThirdWeek/Tree_112.java
+++ src/main/java/com/leetcode/Tree/ThirdWeek/Tree_112.java
@@ -1,126 +1,126 @@
-package com.leetcode.Tree.ThirdWeek;
-
-import com.leetcode.Tree.SecondWeek.Tree_257_BackTracking;
-import com.leetcode.Tree.TreeNode;
-import javafx.util.Pair;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
-public class Tree_112 {
-    boolean judge = false;
-    /**
-     * 1、明确递归函数的参数和返回值 <br>
-     * 参数：node path（记录每一条路径） result（存放结果集）
-     * 返回值：无
-     *
-     * 2、明确终止条件 <br>
-     * 遇到叶子节点
-     *
-     * 3、单次递归逻辑 <br>
-     * 1）终止条件的内部逻辑：应该把path(List<Integer>)转化为allPath中的内容
-     * 2）单次递归逻辑：对左右子树进行递归和回溯
-     *
-     * @param root 树的根节点
-     * @return 树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。<br>
-     *         如果存在，返回 true ；否则，返回 false 。
-     */
-//    public boolean hasPathSum(TreeNode root, int targetSum) {
-//        if (root == null) {
-//            return false;
-//        }
-//        traversal(root, 0, targetSum);
-//        return judge;
-//    }
+//package com.leetcode.Tree.ThirdWeek;
 //
+//import com.leetcode.Tree.SecondWeek.Tree_257_BackTracking;
+//import com.leetcode.Tree.TreeNode;
+//import javafx.util.Pair;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Stack;
+//
+//public class Tree_112 {
+//    boolean judge = false;
 //    /**
-//     * 递归遍历树的每一个节点，当遍历完一个路径之后，则可以退出递归遍历栈
-//     * @param root 树的节点
-//     * @param path 树在该路径上的所有节点val之和
+//     * 1、明确递归函数的参数和返回值 <br>
+//     * 参数：node path（记录每一条路径） result（存放结果集）
+//     * 返回值：无
+//     *
+//     * 2、明确终止条件 <br>
+//     * 遇到叶子节点
+//     *
+//     * 3、单次递归逻辑 <br>
+//     * 1）终止条件的内部逻辑：应该把path(List<Integer>)转化为allPath中的内容
+//     * 2）单次递归逻辑：对左右子树进行递归和回溯
+//     *
+//     * @param root 树的根节点
+//     * @return 树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。<br>
+//     *         如果存在，返回 true ；否则，返回 false 。
 //     */
-//    public void traversal(TreeNode root, int path, int targetSum) {
-//        path += root.val;
-//
-//        if (root.left == null && root.right == null) {
-//            if (path == targetSum) {
-//                judge = true;
-//            }
-//        }
-//
-//        if (root.left != null) {
-////            path += root.left.val;
-//            traversal(root.left, path, targetSum);
-//            // 回溯和递归是一一对应的，有一个递归，就要有一个回溯
-////            path -= root.left.val;
-//        }
-//
-//        if (root.right != null) {
-////            path += root.right.val;
-//            // 回溯和递归是一一对应的，有一个递归，就要有一个回溯
-//            traversal(root.right, path, targetSum);
-////            path -= root.right.val;
-//        }
-//
-//    }
+////    public boolean hasPathSum(TreeNode root, int targetSum) {
+////        if (root == null) {
+////            return false;
+////        }
+////        traversal(root, 0, targetSum);
+////        return judge;
+////    }
+////
+////    /**
+////     * 递归遍历树的每一个节点，当遍历完一个路径之后，则可以退出递归遍历栈
+////     * @param root 树的节点
+////     * @param path 树在该路径上的所有节点val之和
+////     */
+////    public void traversal(TreeNode root, int path, int targetSum) {
+////        path += root.val;
+////
+////        if (root.left == null && root.right == null) {
+////            if (path == targetSum) {
+////                judge = true;
+////            }
+////        }
+////
+////        if (root.left != null) {
+//////            path += root.left.val;
+////            traversal(root.left, path, targetSum);
+////            // 回溯和递归是一一对应的，有一个递归，就要有一个回溯
+//////            path -= root.left.val;
+////        }
+////
+////        if (root.right != null) {
+//////            path += root.right.val;
+////            // 回溯和递归是一一对应的，有一个递归，就要有一个回溯
+////            traversal(root.right, path, targetSum);
+//////            path -= root.right.val;
+////        }
+////
+////    }
+////    public boolean hasPathSum(TreeNode root, int targetSum) {
+////        if (root == null) {
+////            return false;
+////        }
+////        // 叶子结点
+////        if (root.left == null && root.right == null) {
+////            // 且叶子结点的值==修改过后的targetSum
+////            if (root.val == targetSum) {
+////                return true;
+////            }
+////            return false;
+////        }
+////        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+////    }
 //    public boolean hasPathSum(TreeNode root, int targetSum) {
 //        if (root == null) {
 //            return false;
 //        }
-//        // 叶子结点
-//        if (root.left == null && root.right == null) {
-//            // 且叶子结点的值==修改过后的targetSum
-//            if (root.val == targetSum) {
+//
+//        Stack<Pair<TreeNode, Integer>> record = new Stack<>();
+//        record.push(new Pair<>(root, targetSum - root.val));
+//        while (!record.isEmpty()) {
+//            Pair<TreeNode, Integer> node = record.pop();
+//            // 右
+//            TreeNode right = node.getKey().right;
+//            TreeNode left = node.getKey().left;
+//
+//            // 叶子结点
+//            if (right == null && left == null && node.getValue() == 0) {
 //                return true;
 //            }
-//            return false;
+//
+//            if (right != null) {
+//                record.push(new Pair<>(right, node.getValue() - right.val));
+//            }
+//            // 左
+//            if (left != null) {
+//                record.push(new Pair<>(left, node.getValue() - left.val));
+//            }
 //        }
-//        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+//        return false;
 //    }
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null) {
-            return false;
-        }
-
-        Stack<Pair<TreeNode, Integer>> record = new Stack<>();
-        record.push(new Pair<>(root, targetSum - root.val));
-        while (!record.isEmpty()) {
-            Pair<TreeNode, Integer> node = record.pop();
-            // 右
-            TreeNode right = node.getKey().right;
-            TreeNode left = node.getKey().left;
-
-            // 叶子结点
-            if (right == null && left == null && node.getValue() == 0) {
-                return true;
-            }
-
-            if (right != null) {
-                record.push(new Pair<>(right, node.getValue() - right.val));
-            }
-            // 左
-            if (left != null) {
-                record.push(new Pair<>(left, node.getValue() - left.val));
-            }
-        }
-        return false;
-    }
-
-    public static void main(String[] args) {
-//        TreeNode root_left_left = new TreeNode(4);
-//        TreeNode root_left_right = new TreeNode(5);
-//        TreeNode root_left = new TreeNode(2, root_left_left, root_left_right);
-        TreeNode root_left = new TreeNode(1, null, null);
-
-//        TreeNode root_right_left = new TreeNode(6);
-//        TreeNode root_right_right = new TreeNode(7);
-//        TreeNode root_right = new TreeNode(3, root_right_left, root_right_right);
-        TreeNode root_right = new TreeNode(1, null, null);
-
-        TreeNode root = new TreeNode(0, root_left, root_right);
-
-        Tree_112 tree_94 = new Tree_112();
-        boolean result  = tree_94.hasPathSum(root, 1);
-        System.out.println(result);
-    }
-}
+//
+//    public static void main(String[] args) {
+////        TreeNode root_left_left = new TreeNode(4);
+////        TreeNode root_left_right = new TreeNode(5);
+////        TreeNode root_left = new TreeNode(2, root_left_left, root_left_right);
+//        TreeNode root_left = new TreeNode(1, null, null);
+//
+////        TreeNode root_right_left = new TreeNode(6);
+////        TreeNode root_right_right = new TreeNode(7);
+////        TreeNode root_right = new TreeNode(3, root_right_left, root_right_right);
+//        TreeNode root_right = new TreeNode(1, null, null);
+//
+//        TreeNode root = new TreeNode(0, root_left, root_right);
+//
+//        Tree_112 tree_94 = new Tree_112();
+//        boolean result  = tree_94.hasPathSum(root, 1);
+//        System.out.println(result);
+//    }
+//}
---
+++ src/main/java/com/leetcode/dp/Sword_Offer_2_103.java
@@ -0,0 +1,65 @@
+package com.leetcode.dp;
+
+import java.util.Arrays;
+
+public class Sword_Offer_2_103 {
+
+//    int j = 1;
+//    public int coinChange(int[] coins, int amount) {
+//        if (amount == 0) return 0;
+//        Arrays.sort(coins);
+//        int i = amount;
+//        int num = 0;
+//        while (true) {
+//            while (i < coins[coins.length - j]) {
+//                if (j + 1 <= coins.length) {
+//                    j++;
+//                } else break;
+//            }
+//            i -= coins[coins.length - j];
+//            num++;
+//            if (i == 0) {
+//                break;
+//            } else if (i < 0) {
+//                return -1;
+//            }
+//        }
+//        return num;
+//    }
+
+    public int coinChange(int[] coins, int amount) {
+        int[] coinNums = new int[amount + 1];
+        int[] remainder = new int[amount + 1];
+        for (int i = 0; i <= amount; i++) {
+            coinNums[i] = i;
+            remainder[i] = i;
+        }
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i && coinNums[i - coins[j]] + 1 <= coinNums[i]) {
+                    remainder[i] = remainder[i - coins[j]];
+                    coinNums[i] = coinNums[i - coins[j]] + 1;
+                }
+            }
+        }
+        if (remainder[amount] != 0) return -1;
+        else return coinNums[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {186, 419, 83, 408};
+        int amount = 6249;
+
+//        int[] coins = {1, 2, 5};
+//        int amount = 11;
+
+//        int[] coins = {2};
+//        int amount = 3;
+
+//        int[] coins = {1};
+//        int amount = 1;
+
+        Sword_Offer_2_103 main = new Sword_Offer_2_103();
+        System.out.println(main.coinChange(coins, amount));
+    }
+}
