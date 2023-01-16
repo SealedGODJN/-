049f3bb30a90f030b7259332b8b090796a33d4a1
SealedGodJn <1286039722@qq.com>
2023-01-16 19:02:42 +0800

    leetcode+javafx多线程+可视化
    已完成：
    1、学习java多线程3.3.2
    2、总结01背包的遍历顺序、初始化（从定义出发）
    3、leetcode518：零钱兑换（求完全背包的组合和）【优化代码】
    
    待完成：
    1、学习java的多线程
    2、学习乐观锁、悲观锁的具体应用（预计4个小时）

--- src/main/java/com/leetcode/dp/dp_518.java
+++ src/main/java/com/leetcode/dp/dp_518.java
@@ -39,10 +39,8 @@ public class dp_518 {
         // 初始化dp数组
         dp[0] = 1;
         for (int i = 0; i < coins.length; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (j >= coins[i]) {
-                    dp[j] += dp[j - coins[i]];
-                }
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] += dp[j - coins[i]];
             }
         }
         return dp[amount];
