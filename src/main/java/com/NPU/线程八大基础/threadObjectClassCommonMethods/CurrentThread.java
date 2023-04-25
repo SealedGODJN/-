dcaf8aa622e48040d7d730a4de86ace7d9db619d
SealedGODJN <1286039722@qq.com>
2023-04-25 11:01:44 +0800

    leetcode+java多线程基础+可视化
    已完成：
    1、学习scala
    2、【完成】java多线程的Thread、Object类的方法【4-5/8】：总结（wait/notify/sleep/join/yield/start/Thread.currentThread()）
    3、【正在学习】java多线程-线程各个属性【5-1/8】：线程的ID、线程的唯一身份识别号、线程ID、守护线程、线程优先级
    
    待完成：
    1、优化网络演算法

--- src/main/java/com/NPU/线程八大基础/threadObjectClassCommonMethods/CurrentThread.java
+++ src/main/java/com/NPU/线程八大基础/threadObjectClassCommonMethods/CurrentThread.java
@@ -1,8 +1,12 @@
 package com.NPU.线程八大基础.threadObjectClassCommonMethods;
 
+/**
+ * @description 打印main, Thread-0, Thread-1
+ */
 public class CurrentThread implements Runnable {
 
     public static void main(String[] args) {
+        // 打印main的名字
         new CurrentThread().run();
     }
 
