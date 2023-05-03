3e32602f327d1577de2d157e8631fdcb342ac4ad
SealedGODJN <1286039722@qq.com>
2023-05-03 17:32:35 +0800

    1、用更简洁的方法完成1791星形图中心的判断（题目条件：给出的图一定是星形图）

--- leetcode/graph/degree/1791_Find_Center_of_Star_Graph.cpp
+++ leetcode/graph/degree/1791_Find_Center_of_Star_Graph.cpp
@@ -13,19 +13,23 @@ public:
         
     */
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size() + 1;
-        vector<int> Degrees(n + 1);
-        for (vector<int> edge : edges) {
-            int start = edge[0];
-            int end = edge[1];
-            Degrees[end]++;
-            Degrees[start]++;
-        }
-        for (int i = 0; i <= n; i++) {
-            if (Degrees[i] == n - 1)
-                return i;
-        }
+        // 判断edges[0][0]是否出现了两次？
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
     }
+    // int findCenter(vector<vector<int>>& edges) {
+    //     int n = edges.size() + 1;
+    //     vector<int> Degrees(n + 1);
+    //     for (vector<int> edge : edges) {
+    //         int start = edge[0];
+    //         int end = edge[1];
+    //         Degrees[end]++;
+    //         Degrees[start]++;
+    //     }
+    //     for (int i = 0; i <= n; i++) {
+    //         if (Degrees[i] == n - 1)
+    //             return i;
+    //     }
+    // }
 };
 
 int main()
