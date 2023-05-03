586e00bd84c89172123579b8e402513c78e5c7c0
SealedGODJN <1286039722@qq.com>
2023-05-03 16:46:53 +0800

    1、leetcode构建模式识别。遇到题目，先分清是什么类型，再看是考什么内容？最后开始读题，标注题目的重点内容

---
+++ leetcode/graph/degree/1791_Find_Center_of_Star_Graph.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    /*
+        易错点：
+        1、边的数量和图的顶点数量之间的关系
+        2、无向图还是有向图？
+        3、考点？出度入度？还是DFS、BFS、并查集？
+        
+    */
+    int findCenter(vector<vector<int>>& edges) {
+        int n = edges.size() + 1;
+        vector<int> Degrees(n + 1);
+        for (vector<int> edge : edges) {
+            int start = edge[0];
+            int end = edge[1];
+            Degrees[end]++;
+            Degrees[start]++;
+        }
+        for (int i = 0; i <= n; i++) {
+            if (Degrees[i] == n - 1)
+                return i;
+        }
+    }
+};
+
+int main()
+{
+    vector<vector<int>> trust = {{1,2},{2,3},{4,2}};
+    Solution s;
+    cout << s.findCenter(trust);
+    cin.get();
+}
