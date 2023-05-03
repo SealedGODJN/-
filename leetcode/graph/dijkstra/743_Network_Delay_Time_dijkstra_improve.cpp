dbec56ef6fdbb88ace96d3d87886821fd403cfd4
SealedGODJN <1286039722@qq.com>
2023-05-03 21:38:22 +0800

    1、dijsktra算法基于贪心策略（每次都选择minCost中cost最小且未访问过的节点）

--- leetcode/graph/dijkstra/743_Network_Delay_Time_dijkstra_improve.cpp
+++ leetcode/graph/dijkstra/743_Network_Delay_Time_dijkstra_improve.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 #define typec int
 
 const typec INF = 0x3f3f3f3f; // 防止后面溢出，这个不能太大
+
+struct qnode {
+    int vertex;
+    int cost;
+    qnode(int _vertex=0,int _cost=0):vertex(_vertex),cost(_cost){}
+    bool operator <(const qnode &r) const {
+        return cost > r.cost;
+    }
+};
 
 class Solution
 {
@@ -34,38 +44,38 @@ public:
             cost[edge[0]][edge[1]] = edge[2];
         }
         // 初始化要用的内容：
+        priority_queue<qnode> que;
+        while(!que.empty())
+            que.pop();
         vector<int> minCost(n + 1);
         vector<bool> isVisited(n + 1);
         for (int i = 1; i < n + 1; i++)
         {
-            minCost[i] = cost[k][i];
+            // 不能初始化minCost
+            // minCost[i] = cost[k][i];
+            minCost[i] = INF;
             isVisited[i] = false;
         }
-        isVisited[k] = true;
+        // isVisited[k] = true;
         minCost[k] = 0;
-        for (int i = 1; i < n + 1; i++)
+
+        que.push(qnode(k, 0));
+        qnode tmp;
+
+        while(!que.empty())
         {
-            // 初始化min
-            int min = INF;
-            // 当前轮循环，节点cur的最短路要被处理
-            int cur = -1;
-            for (int j = 1; j < n + 1; j++)
-            {
-                if (!isVisited[j] && minCost[j] < min)
-                {
-                    min = minCost[j];
-                    cur = j;
-                }
-            }
-            if (cur == -1)
-                break;
+            tmp = que.top();
+            que.pop();
+            int cur = tmp.vertex;
+            if (isVisited[cur]) continue;
             isVisited[cur] = true;
-            for (int j = 1; j < n + 1; j++)
+            for (int j = 1; j < cost[cur].size(); j++)
             {
                 if (!isVisited[j] && minCost[cur] + cost[cur][j] < minCost[j])
                 {
                     // 更新每一个节点的minCost
                     minCost[j] = minCost[cur] + cost[cur][j];
+                    que.push(qnode(j, minCost[j]));
                 }
             }
         }
